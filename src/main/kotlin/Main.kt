ef0bcc0c700249484318d96929150e1bf5cc1eb4
hiroaki404 <nosho404@gmail.com>
2025-06-27 18:04:15 +0900

    Add CLI functionality with Clikt library and Shadow plugin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,20 @@
 package org.example
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.main
+import com.github.ajalt.clikt.parameters.options.default
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.types.int
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+class Hello : CliktCommand() {
+    private val count: Int by option(help = "Number of greetings").int().default(1)
+    private val name: String by option(help = "The name to greet").default("World")
+
+    override fun run() {
+        for (i in 1..count) {
+            echo("Hello $name!")
+        }
     }
 }
+
+fun main(args: Array<String>) = Hello().main(args)

b9d6981988a61a58d518f92d9a8b7260145a5a31
hiroaki404 <nosho404@gmail.com>
2025-06-27 22:36:27 +0900

    Migrate build scripts from Groovy DSL to Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    kotlin("jvm") version "2.1.21"
+    application
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("com.github.ajalt.clikt:clikt:5.0.3")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(17)
+}
+
+application {
+    mainClass.set("org.example.MainKt")
+}

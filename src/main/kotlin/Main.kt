77e389b64f9642453281b1b886b468bdce204391
hiroaki404 <nosho404@gmail.com>
2025-06-28 14:37:31 +0900

    Refactor command structure to implement translation functionality

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,19 +2,20 @@ package org.example
 
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.core.main
-import com.github.ajalt.clikt.parameters.options.default
-import com.github.ajalt.clikt.parameters.options.option
-import com.github.ajalt.clikt.parameters.types.int
+import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.arguments.optional
 
-class Hello : CliktCommand() {
-    private val count: Int by option(help = "Number of greetings").int().default(1)
-    private val name: String by option(help = "The name to greet").default("World")
+class GenTransCommand : CliktCommand() {
+    private val targetText: String? by argument(help = "Text to translate. Reads from stdin if not provided.").optional()
 
     override fun run() {
-        for (i in 1..count) {
-            echo("Hello $name!")
+        val text = targetText ?: run {
+            val stdinText = generateSequence(::readlnOrNull).joinToString("\n")
+            stdinText
         }
+        echo("Translating: $text")
+        // Here you would add the actual translation logic
     }
 }
 
-fun main(args: Array<String>) = Hello().main(args)
+fun main(args: Array<String>) = GenTransCommand().main(args)

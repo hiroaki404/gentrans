55dd9753f2b5f57f8b5eeaaa560e9ff44328de51
hiroaki404 <nosho404@gmail.com>
2025-06-29 22:09:01 +0900

    feat: remove clikt from test dependencies and enhance GenTransCommand tests for stdin input

--- src/test/kotlin/MyFirstTest.kt
+++
@@ -1,8 +0,0 @@
-import io.kotest.core.spec.style.StringSpec
-import io.kotest.matchers.shouldBe
-
-class MyFirstTest : StringSpec({
-    "length should return the length of a string" {
-        "hello".length shouldBe 5
-    }
-})
